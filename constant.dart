f73012fbd2b99ba62f67e44df12defa79f8f233b
Gohar Ali Bhatti <84314963+goharali411@users.noreply.github.com>
2024-11-27 10:47:28 +0500

    Add files via upload
    Features ✨
    Arithmetic Operations: Perform addition, subtraction, multiplication, division, and percentage calculations.
    Utility Functions:
    AC: Clears all input and resets the calculator.
    DEL: Deletes the last input character.
    +/-: Toggles between positive and negative values.
    Dynamic Input and Result Display:
    Real-time display of user input.
    Immediate evaluation of results with the = button.
    Responsive Design: Adapts seamlessly to different screen sizes.
    Modern UI: Clean and visually appealing design using custom widgets.
    Technologies Used 💻
    Flutter: Framework for crafting beautiful and natively compiled applications.
    Dart: The programming language powering the app's business logic.
    Math Expressions Package: For parsing and evaluating mathematical expressions.
    Core Logic 🧠
    Expression Evaluation: The math_expressions package processes user input as a string, parses it, and evaluates the result dynamically.
    State Management: Utilized Flutter's StatefulWidget to manage the calculator's dynamic behavior.
    Widgets Overview 🧩
    MyButton (Custom Widget):
    Reusable component for all calculator buttons.
    Features customizable color, size, and functionality using InkWell.
    HomeScreen:
    The main calculator interface.
    Built with a column layout to separate input/result display and buttons.
    Scaffold and SafeArea: For consistent app structure and avoiding unsafe areas on devices.
    How It Works ⚙️
    Input Management:
    Users interact with buttons to form a mathematical expression.
    Inputs are dynamically displayed on the screen.
    Calculation Logic:
    When the = button is pressed, the app evaluates the entered expression using the Parser and ContextModel from the math_expressions package.
    Error Handling:
    Handles invalid expressions gracefully and displays an appropriate error message.

---
+++ constant.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color whiteColor = Colors.white;
+const Color blackColor = Colors.black;
+
+const headingTextStyle =
+    TextStyle(fontSize: 20, color: Colors.white, fontWeight: FontWeight.w500);
